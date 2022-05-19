caa405d5841182452578a07c2097ae7d82d04c3e
dargy <76412158+dragonismcode@users.noreply.github.com>
2022-05-19 18:51:01 -0500

    Adding local storage and channel URLS

--- script.js
+++ script.js
@@ -4,43 +4,49 @@ var odometer = document.getElementById("odometer");
 var titletext = document.getElementById("titletext");
 var authortext = document.getElementById("authortext");
 
-var rumbleUser = { "id": "_c276782", "slug": "Bongino", "title": "The Dan Bongino Show", "type": "channel", "name": "Bongino" }
+if(!localStorage.getItem("rumbleUser")) {
+  localStorage.setItem("rumbleUser", JSON.stringify({
+    id: "_c276782",
+    slug: "Bongino",
+    title: "The Dan Bongino Show",
+    type: "channel",
+    name: "Bongino",
+  }));
+}
 
-var refreshInterval = setInterval(refresher,2000)
+var refreshInterval = setInterval(refresher, 2000);
 loadCounter();
 
 async function loadId() {
   var response = prompt(
     "Please enter the user you want to track's hanlde in the prompt down below."
   );
-  if (!response) return console.log("User cancellled prompt... that's pretty sad.")
+  if (!response)
+    return console.log("User cancellled prompt... that's pretty sad.");
 
-  await fetch(
-    "https://api.rumblecounter.live/search/users?query=" +
-    response
-  ).then(res => res.text())
-  .then(data => {
-    console.log(data)
-    if (data !== "not found!") {
-      data = JSON.parse(data)
-      rumbleUser.id = data[0].id;
-      clearInterval(refreshInterval)
-      loadCounter()
-    } else {
-      alert('No users found.')
-    }
-  })
+  await fetch("https://api.rumblecounter.live/search/users?query=" + response)
+    .then((res) => res.text())
+    .then((data) => {
+      console.log(data);
+      if (data !== "not found!") {
+        data = JSON.parse(data);
+        localStorage.setItem("rumbleUser", JSON.stringify(data[0]));
+        titletext.innerText = data[0].title;
+        titletext.href = `https://rumble.com/c/${data[0].slug}`
+        clearInterval(refreshInterval);
+        loadCounter();
+      } else {
+        alert("No users found.");
+      }
+    });
 }
 
 // can you also make the odermeter bigger?
 //k
 
 function loadCounter() {
-  fetch(
-    "https://api.rumblecounter.live/user?id=" +
-    rumbleUser.id
-  )
-    .then(function(response) {
+  fetch("https://api.rumblecounter.live/user?id=" + JSON.parse(localStorage.getItem("rumbleUser")).id)
+    .then(function (response) {
       //The API call was successful!
       if (response.ok) {
         return response.json();
@@ -49,39 +55,32 @@ function loadCounter() {
       }
       //return response;
     })
-    .then(function(res) {
+    .then(function (res) {
       odometer.innerHTML = res.followers;
       titletext.innerText = res.title;
-      rumbleUser.title = res.title;
-      rumbleUser.slug = res.slug;
-      rumbleUser.name = res.name;
+      titletext.href = `https://rumble.com/c/${res.slug}`
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // There was an error
       console.warn("Something went wrong.", err);
     });
 }
 
-  function refresher() {
-    fetch("https://api.rumblecounter.live/user?id=" +
-      rumbleUser.id)
-      .then(function(response) {
-        // The API call was successful!
-        if (response.ok) {
-          return response.json();
-        } else {
-          return Promise.reject(response);
-        }
-      })
-      .then(function(res) {
+function refresher() {
+  fetch("https://api.rumblecounter.live/user?id=" + JSON.parse(localStorage.getItem("rumbleUser")).id)
+    .then(function (response) {
+      // The API call was successful!
+      if (response.ok) {
+        return response.json();
+      } else {
+        return Promise.reject(response);
+      }
+    })
+    .then(function (res) {
       odometer.innerHTML = res.followers;
-      titletext.innerText = res.title;
-      rumbleUser.title = res.title;
-      rumbleUser.slug = res.slug;
-      rumbleUser.name = res.name;
-      })
-      .catch(function(err) {
-        // There was an error
-        console.warn("Something went wrong.", err);
-      });
-  }
+    })
+    .catch(function (err) {
+      // There was an error
+      console.warn("Something went wrong.", err);
+    });
+}
